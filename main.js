d274d55a80824ac3fd1afb3449777f5cb99d72f9
Alexis Villegas Torres <alexisvt@gmail.com>
2019-06-19 22:30:36 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    inventory: 9,
+    onSale: true,
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+      },
+    ],
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+  },
+});
