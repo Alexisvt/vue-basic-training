125953adb3c1be6933ffc9f4925636fbf7c501d3
Alexis Villegas Torres <alexisvt@gmail.com>
2019-06-19 22:36:04 -0600

    adding a small change into the app

--- main.js
+++ main.js
@@ -9,10 +9,12 @@ var app = new Vue({
       {
         variantId: 2234,
         variantColor: 'green',
+        variantImage: './assets/vmSocks-green-onWhite.jpg',
       },
       {
         variantId: 2235,
         variantColor: 'blue',
+        variantImage: './assets/vmSocks-blue-onWhite.jpg',
       },
     ],
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
